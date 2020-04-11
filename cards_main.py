fa0629e7e0514c997feae812e418043a57eba7f7
zhaiyujia <zhaiyujia2020@gmail.com>
2020-04-12 01:50:36 +0800

    4-1.Bug修改1-使用int转换用户输入 → 不使用int转换用户输入，避免一旦用户输入的不是数字，导致程序运行出错[翟羽佳/2020年4月12日]

--- cards_main.py
+++ cards_main.py
@@ -3,22 +3,22 @@ while True:
     # TODO（翟羽佳） 显示功能菜单
 
     # 获取用户输入的操作选项
-    option_num = int(input("请输入您想执行操作的序号："))
-    print("您选择的操作是【%d】" % option_num)
+    option_str = input("请输入您想执行操作的序号：")
+    print("您选择的操作是【%s】" % option_str)
 
     # 输入1、2、3，执行相应的名片操作
-    if option_num in [1, 2, 3]:
+    if option_str in ["1", "2", "3"]:
         # TODO(翟羽佳) 1：新建名片
-        if option_num == 1:
+        if option_str == "1":
             pass
         # TODO（翟羽佳） 2：显示全部
-        elif option_num == 2:
+        elif option_str == "2":
             pass
         # TODO（翟羽佳） 3：查询名片
-        elif option_num == 3:
+        elif option_str == "3":
             pass
     # 输入0，退出名片系统
-    elif option_num == 0:
+    elif option_str == "0":
         print("欢迎再次使用【名片管理系统】")
         break
     # 输入其他内容，提示用户输入错误
