fe449a3d0cd48e2a9e9ce0a381c7c04b88eb9dba
Xiang Gu <guxiang@rd.netease.com>
2020-04-12 01:20:03 +0800

    3.框架搭建-无限循环，由用户主动决定何时退出循环[翟羽佳/2020年4月12日]

--- cards_main.py
+++ cards_main.py
@@ -1,13 +1,15 @@
-# 获取用户输入的操作选项
-option_num = int(input("请输入您想执行操作的序号："))
-print("您选择的操作是【%d】" % option_num)
+while True:
+    # 获取用户输入的操作选项
+    option_num = int(input("请输入您想执行操作的序号："))
+    print("您选择的操作是【%d】" % option_num)
 
-# 输入1、2、3，执行相应的名片操作
-if option_num in [1, 2, 3]:
-    pass
-# 输入0，退出名片系统
-elif option_num == 0:
-    pass
-# 输入其他内容，提示用户输入错误
-else:
-    print("你输入的不正确，请重新选择")
+    # 输入1、2、3，执行相应的名片操作
+    if option_num in [1, 2, 3]:
+        pass
+    # 输入0，退出名片系统
+    elif option_num == 0:
+        print("欢迎再次使用【名片管理系统】")
+        break
+    # 输入其他内容，提示用户输入错误
+    else:
+        print("你输入的不正确，请重新选择")
